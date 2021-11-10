61e1451374ba48900fcfc86ce6a033c1f6cbe0c6
Aymen <hench.aymen@gmail.com>
2021-11-10 16:42:13 +0100

    adding react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Form, Button, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="me-auto">
+        <Nav>
+          <Nav.Link href="#iphone">Apple</Nav.Link>
+          <Nav.Link href="#samsung">Samsung</Nav.Link>
+          <Nav.Link href="#huawei">Huawei</Nav.Link>
+          <Nav.Link href="#oppo">Oppo</Nav.Link>
+          <Nav.Link href="#Xiaomi">Xiaomi</Nav.Link>
+        </Nav>
       </header>
+      <main>
+        <h1 className="title"> Phone Shop </h1>
+        <Form className="form">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Sign In
+          </Button>
+        </Form>
+      </main>
+      <footer className="me-auto">
+        <div class="footer">
+          © 2021 Copyright : Phoneshop.com created by H.Aymen
+        </div>
+      </footer>
     </div>
   );
 }
